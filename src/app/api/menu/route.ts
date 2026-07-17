d734df5a94a8831730a038f5fe1949c96aa1fbe0
Douae <douaejanane1@gmail.com>
2026-07-17 19:29:40 +0100

    Add files via upload

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getCategories, getMenuItems } from "@/lib/db/repo";
 
 export async function GET() {
-  const categories = getCategories();
-  const items = getMenuItems();
+  const categories = await getCategories();
+  const items = await getMenuItems();
   return NextResponse.json({ categories, items });
 }
